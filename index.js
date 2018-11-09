2e028a187976560ec5f718afd4180aed29153cdf
Sergio Neves <serneves@hotmail.com>
2018-11-09 19:58:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function theBeatlesPlay (musicians, instruments) {
   var beatles = []
 
   for(let i = musicians[0];i = musicians.lenght; i = musicians[i++]) {
-    beatles.push(musicians[i]+instruments[i])
+    beatles.push("${musicians} plays ${instruments}")
   }
-  
+
   return beatles
 }

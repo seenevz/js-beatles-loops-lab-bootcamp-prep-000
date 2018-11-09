8b8b5684e96600ed655a42a2d7fb904d16c164a5
Sergio Neves <serneves@hotmail.com>
2018-11-09 20:33:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,15 @@ function johnLennonFacts (facts) {
   }
   return newFacts
 }
+
+function iLoveTheBeatles (n) {
+  var array = []
+
+  do {
+    array.push("I love the Beatles!")
+    n++
+  }
+  while(n<15)
+
+  return array
+}

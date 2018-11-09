8dc5222ebe827a1993ed88994cf46360d1333773
Sergio Neves <serneves@hotmail.com>
2018-11-09 19:35:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 
 
-function theBeatlesPlay (musicians, instruments) {
-  var beatles = []
-
-  for (musicians[0];; musicians++) {
-    beatles.push(musicians+instruments)
-  }
-  return beatles
-}
